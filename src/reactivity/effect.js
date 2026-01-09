57e3f897109a80d0178540ea905d839b3c4134c4
Jack Pillai <aarryann@yahoo.com>
2026-01-09 14:07:47 -0500

    reduce deps

--- src/reactivity/effect.js
+++ src/reactivity/effect.js
@@ -1,3 +1,5 @@
+import { targetMap } from "./reactive.js";
+
 export let activeEffect = null
 
 export function effect1(source, run, {defer = true} = {}) {
@@ -16,10 +18,89 @@ export function effect1(source, run, {defer = true} = {}) {
   runner();
 }
 
-export function effect(run) {
+export function effect2(run) {
   console.log(`Registering effect 2`);
   activeEffect = run;
   run();
   activeEffect = null;
 
+}
+
+export function cleanup(effectFn) {
+  if (!effectFn.deps) return;
+  for (const dep of effectFn.deps) {
+    dep.delete(effectFn);
+  }
+  effectFn.deps.length = 0;
+}
+
+/**
+ * 
+ * Mode 1: Auto inference - effect(fn, opts) = effect(() => { lbl.textContent = state.user }, {})
+ * Mode 2: Optimized with getters - effect(fn, opts) = effect(([tag, status])=>{lbl.textContent = `The status of ${tag} is ${status}`}, {defer: true, deps:[() => card.tag, () => card.status]})
+ * Mode 3: Optimized with value tuples - effect(fn, opts) = effect(([tag, status])=>{lbl.textContent = `The status of ${tag} is ${status}`}, {defer: true, deps:[[card, "tag"], [card, "status"]]})
+ * 
+ */
+export function effect(fn, opts = {}) {
+  const {
+    deps = null,
+    lazy = false,
+    scheduler = null
+  } = opts;
+  let init = true;
+
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+
+    let values = [];
+
+    // MODE 1: auto inference
+    if (!deps) {
+      if (!(lazy && init)) fn();
+    }
+
+    // MODE 2: getter deps
+    else if (typeof deps[0] === "function") {
+      values = deps.map(getter => getter());
+      activeEffect = null;
+      if (!(lazy && init)) fn(values);
+    }
+
+    // MODE 3: tuple deps
+    else if (Array.isArray(deps[0])) {
+      values = deps.map(([target, key]) => {
+        let depsMap = targetMap.get(target);
+        if (!depsMap) {
+          depsMap = new Map();
+          targetMap.set(target, depsMap);
+        }
+
+        let dep = depsMap.get(key);
+        if (!dep) {
+          dep = new Set();
+          depsMap.set(key, dep);
+        }
+
+        if (!dep.has(effectFn)) {
+          dep.add(effectFn);
+          effectFn.deps.push(dep);
+        }
+
+        return target[key];
+      })
+      activeEffect = null
+      if (!(lazy && init)) fn(values);
+    }
+    activeEffect = null
+    init = false;
+  }
+  effectFn.deps = []
+  effectFn.scheduler = scheduler
+
+  if (!lazy) {
+    effectFn()
+  }
+
+  return effectFn  
 }
--- src/reactivity/reactive.js
+++ src/reactivity/reactive.js
@@ -1,27 +1,15 @@
 import { activeEffect } from "./effect.js"
 
-//export const deps = new WeakMap()
 const proxyCache = new WeakMap()
 export const targetMap = new WeakMap();
 export const debugKeys = new Set();
 const IS_REACTIVE = Symbol("isReactive")
 
-/*
-function track1(target, key) {
-  if (!activeEffect) return
-  let depMap = deps.get(target)
-  if (!depMap) deps.set(target, depMap = new Map())
-  let dep = depMap.get(key)
-  if (!dep) depMap.set(key, dep = new Set())
-  dep.add(activeEffect)
-}
-*/
-
-export function track(target, key) {
+function track(target, key) {
   if (!activeEffect) return;
 
   debugKeys.add(target);
-  console.log("track →", target, key);
+  //console.log("track →", target, key);
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -33,58 +21,59 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
-  dep.add(activeEffect)
+  if (!dep.has(activeEffect)) {
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep) 
+  }
 }
 
-
-/*
-function trigger1(target, key) {
-  console.log("TRIGGER", key)
-  deps.get(target)?.get(key)?.forEach(fn => fn())
-}
-*/
-
-export function trigger(target, key) {
+function trigger(target, key) {
   const depsMap = targetMap.get(target);
   if (!depsMap) return;
 
-  console.log("trigger →", target, key);
+  //console.log("trigger →", target, key);
   const effects = depsMap.get(key);
   if (effects) {
-    effects.forEach(effect => effect());
+    effects.forEach(effect => {
+      if (effect.scheduler) {
+        effect.scheduler(effect);
+      } else {
+        effect();
+      }
+    })
   }
 }
 
 
 export function reactive(obj) {
-  if (typeof obj !== "object" || obj === null) return obj
+  if (typeof obj !== "object" || obj === null) return obj;
 
-  if (obj[IS_REACTIVE]) return obj
+  if (obj[IS_REACTIVE]) return obj;
 
-  const existing = proxyCache.get(obj)
-  if (existing) return existing
+  const cached = proxyCache.get(obj);
+  if (cached) return cached;
 
   const proxy = new Proxy(obj, {
     get(target, key, receiver) {
-      if (key === IS_REACTIVE) return true
+      if (key === IS_REACTIVE) return true;
       //console.log(`Getting reactive value for ${key}`);
-      track(target, key)
-      const res = Reflect.get(target, key, receiver)
+      track(target, key);
+      const res = Reflect.get(target, key, receiver);
       return typeof res === 'object' && res !== null
         ? reactive(res)
         : res;      
     },
     set(target, key, value, receiver) {
       const old = target[key];
-      const result = Reflect.set(target, key, value, receiver)
+      const result = Reflect.set(target, key, value, receiver);
       if (old !== value) {
         trigger(target, key);
       }
       return result
     },
     deleteProperty(target, key) {
-      const result = Reflect.deleteProperty(target, key)
-      trigger(target, key)
+      const result = Reflect.deleteProperty(target, key);
+      trigger(target, key);
       return result
     }    
   })
